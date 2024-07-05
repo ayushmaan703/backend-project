1608467c22c498f2cd48e5dcfa5aa4a6459b9124
Ayushmaan <ayushmaankumar5@gmail.com>
2024-07-05 12:12:05 +0530

    for deployment process

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import morgan from "morgan"
 const app = express()
 
 app.use(
@@ -13,6 +14,10 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(morgan("dev"))
+app.use("/", (req, res) => {
+    res.send({ message: "Welcome to videotube" })
+})
 
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
